0a951dec3689cc6ef16a630f1e106eef5db31e32
Aymane <70668704+Aymane157@users.noreply.github.com>
2026-05-28 22:12:24 +0100

    chore: init mono-repo 2 (#2)

---
+++ project-service/src/main/java/com/jiraclone/project/ProjectServiceApplication.java
@@ -0,0 +1,13 @@
+// project-service/src/main/java/com/jiraclone/project/ProjectServiceApplication.java
+package com.jiraclone.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectServiceApplication.class, args);
+    }
+}
