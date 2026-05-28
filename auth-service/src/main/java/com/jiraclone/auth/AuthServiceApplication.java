d0d2e1be1719077a60d3a06d5de61326861d417b
Aymane157 <aymanemwa@gmail.com>
2026-05-28 21:37:03 +0100

    chore: init mono-repo 2

---
+++ auth-service/src/main/java/com/jiraclone/auth/AuthServiceApplication.java
@@ -0,0 +1,13 @@
+// auth-service/src/main/java/com/jiraclone/auth/AuthServiceApplication.java
+package com.jiraclone.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+}
